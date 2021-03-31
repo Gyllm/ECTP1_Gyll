f0b442af216e2ff59d096bfb5d1534791ccf09ab
Gyllm <60905422+Gyllm@users.noreply.github.com>
2021-03-30 23:43:21 -0300

    Etapa 2

--- src/objs.nim
+++ src/objs.nim
@@ -1,16 +1,17 @@
 type
-  Empresa* = ref object of RootObj
-    cnpj*:string # uma proriedade
-    nomeFantasia*:string #
-    endereco*:string #
-  
-  Fornecedor* = ref object of RootObj #
-    cnae*:string#
+  Fornecedor* = ref object of RootObj
+    nomeFantasia*:string
+    cnpj*:string
+    endereco*:string
+    cnae*:string
 
-  Cliente* = ref object of RootObj #
+  Cliente* = ref object of RootObj
+    nomeFantasia*:string
+    cnpj*:string
+    endereco*:string
     categoria*:string
 
-  Produto* = ref object of RootObj #
+  Produto* = ref object of RootObj
     codigo*:int
     descricao*:string
     valor*:float
--- src/utils.nim
+++ src/utils.nim
@@ -1,17 +1,15 @@
 import objs
-
 import strutils
-import strformat
 
 var fornecedores:seq[Fornecedor]
 var clientes:seq[Cliente]
 var produtos:seq[Produto]
 
+
 proc linha() =
   echo "==========================================================================="
 
-
-proc menu() =
+proc menu*() =
   linha()
   echo """
   ADMIN - V1 - SEU JOAQUIM
@@ -23,7 +21,7 @@ proc menu() =
 	"""
   linha()
 
-proc subMenu() =
+proc subMenu*() =
   linha()
   echo """
   ADMIN - SUBMENU
@@ -34,61 +32,69 @@ proc subMenu() =
   """
   linha()
 
-proc adicionaFornecedor*(Fornecedor) = 
-  var fornecedor:Fornecedor = Fornecedor*()
+proc adicionaFornecedor*() = 
+  var fornecedor = Fornecedor()
   linha()
   echo "CADASTRO FORNECEDOR"
   echo "CNPJ:"
+  fornecedor.cnpj = readLine(stdin)
+  echo "NOME DO FORNECEDOR: "
+  fornecedor.nomeFantasia = readLine(stdin)
+  echo "ENDERECO: "
+  fornecedor.endereco = readLine(stdin)
+  echo "CNAE: "
   fornecedor.cnae = readLine(stdin)
   fornecedores.add(fornecedor)
   echo "Fornecedor cadastrado com sucesso!!"
   linha()
 
-proc adicionaCliente*(Cliente) = 
-  var cliente:Cliente = Cliente*()
+proc adicionaCliente*() = 
   linha()
-  echo "CADASTRO CLIENTE"
-  echo "CATEGORIA"
-  cliente.categoria = readLine(stdin)
-  echo "Cliente cadastrado com sucesso!!"
-  linha()
+  var cliente = Cliente()
+  echo "Bem vindo ao Cadastro de Cliente" 
+  echo "Digite o nome do Cliente"
+  echo "Digite o nome do Cliente"
+  cliente.nomeFantasia=readline(stdin)
+  echo "Digite o CNPJ do Cliente"
+  cliente.cnpj=readline(stdin)
+  echo "Digite o endereco do Cliente"
+  cliente.endereco=readline(stdin)
+  echo "Digite a categoria do Cliente"
+  echo "1- Normal"
+  echo "2- Premium"
+  echo "3- Master"
+  clientes.add(cliente)
 
-proc adicionaProduto*(Produto) = 
-  var produto:Produto = Produto*()
+proc adicionaProduto*() = 
   linha()
-  echo "CADASTRO PRODUTO"
-  echo "Codigo:"
-  produto.codigo = parseInt(readLine(stdin))
-  echo "Descricao:"
-  produto.descricao = readLine(stdin)
-  echo "Valor"
-  produto.valor = parseFloat(readLine(stdin))
+  var produto = Produto()
+  echo "Bem vindo ao Cadastro de Produto" 
+  echo "Digite o código do Produto"
+  produto.codigo= parseInt(readline(stdin))
+  echo "Digite a descrição do Produto"
+  produto.descricao=readline(stdin)
+  echo "Digite o valor do Produto"
+  produto.valor= parseFloat(readline(stdin))
   produtos.add(produto)
   echo "Produto cadastrado com sucesso!!"
   linha()
 
-proc listaFornecedores*(Fornecedor) =
-  for fornecedor in listaFornecedores*()
-    linha()
-    echo ##
-    # 
-    # 
-    # 
-    linha()
+proc listaFornecedores*() =
+  for fornecedor in fornecedores :
+    echo "Nome do fornecedor: " & fornecedor.nomeFantasia
+    echo "CNPJ do Fornecedor: " & fornecedor.cnpj
+    echo "Endereço do Fornecedor: " & fornecedor.endereco
 
-proc listaClientes*(Cliente) =
-  for Cliente in listaClientes*()
-    linha()
-    echo # 
-    # 
-    # 
-    # 
-    linha()
+proc listaClientes*() =
+  for cliente in clientes :
+    echo "Nome do Cliente: " & cliente.nomeFantasia
+    echo "CNPJ do Cliente: " & cliente.cnpj
+    echo "Endereço do Fornecedor: " & cliente.endereco
+    echo "Categoria do Cliente: " & cliente.categoria
 
-proc listaProdutos*(Produto) =
-  for Produto in listaProdutos*()
-    linha()
-  #
-  #
-  #
-    linha()
+proc listaProdutos*() = 
+  for produto in produtos:
+    echo produto.codigo
+    echo produto.valor
+    echo produto.descricao
+
